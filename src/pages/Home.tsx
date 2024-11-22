81b347c85c00d0ec990e4ba0db8dff3e6d29801e
linying <2227580291@qq.com>
2024-11-22 11:17:16 +0800

    feat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ import up from "../assets/up.svg";
 import homeimg1 from "../assets/homeimg1.png";
 import homeimg2 from "../assets/homeimg2.png";
 import caligra3 from "../assets/caligra3.png";
+import strike from "../assets/strike.svg";
 
 const Home: React.FC = () => {
   const navigateTo = useNavigate();
@@ -21,147 +22,229 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex flex-col justify-center">
-      <HomeCompose
-        place="left"
-        onClick={() => navigate("/course1")}
-        imgNode={
-          <HomeImg
-            src={city}
-            title={
-              <>
-                El curso de español
-                <br />
-                ideal para principiantes.
-                <br />
-                最适合初学者入门的西语课程
-              </>
-            }
-            caption={
-              <>
-                ¿Por qué es importante aprender chino mandarín
-                <br />
-                para los niños...
-              </>
-            }
-          />
-        }
-        contentNode={
-          <div className="max-w-[550px] p-8 card shadow-lg text-black">
-            <h1 className="text-3xl font-bold mb-4">
-              Curso de escucha y conversación de español A1 de 4 Semanas
-            </h1>
-            <p className="text-xl text-brown-600 mb-4">€129</p>
-            <div className="flex items-center mb-4">
-              <span className="text-yellow-500">★★★★☆</span>
-              <span className="ml-2 text-gray-500">
-                Ruta de aprendizaje única | Curso en línea
-              </span>
-            </div>
-            <p className="mb-4">
-              Comienza con los auténticos <strong>hábitos lingüísticos</strong>{" "}
-              de los nativos y practica integralmente la{" "}
-              <strong>comprensión auditiva</strong>, la{" "}
-              <strong>expresión oral</strong>, la <strong>lectura</strong> y la{" "}
-              <strong>escritura</strong>. Desde el principio, podrás conversar.
-            </p>
-            <p className="mb-4">
-              <strong>Curso en línea</strong> en grupos pequeños de alta
-              calidad, con un máximo de <strong>10 personas</strong>, donde se
-              supervisan mutuamente en el aprendizaje.
-            </p>
-            <p>
-              Partiendo de <strong>textos de noticias</strong> o{" "}
-              <strong>historias de 10 minutos</strong>, completar en{" "}
-              <strong>cuatro semanas</strong> los temas básicos del nivel A1:
-              <strong> saludar</strong>,{" "}
-              <strong>preguntar sobre el clima</strong>,{" "}
-              <strong>preguntar sobre el estado de salud</strong> y expresar{" "}
-              <strong>actividades</strong> que uno mismo u otras personas están
-              realizando.
-            </p>
-          </div>
-        }
-      />
-      <HomeCompose
-        place="right"
-        onClick={() => navigate("/course2")}
-        imgNode={
-          <HomeImg
-            src={panda}
-            title={
-              <div className="text-[2rem] line-[2rem] text-right">
-                <span>
-                  Aprender chino adaptadoal pensamiento de laslenguas
-                  occidentales.
-                </span>
-                <br />
-                <span>适合西方语言思维的中文学习</span>
-                <br />
-              </div>
-            }
-            caption={<></>}
-          />
-        }
-        contentNode={
-          <div className="max-w-[300px] flex-1 text-black pt-10 pl-10">
-            <div className="text-lg font-bold">
-              Nombre del curso:{" "}
-              <strong>Curso de Introducción al Chino para Principiantes</strong>{" "}
-               520€/人
-            </div>
-            
-            <div className="text-lg mt-2">
-              <strong>Objetivo del curso:</strong> Ayudar a los estudiantes sin{" "}
-              <strong>conocimientos previos</strong> de chino a dominar el{" "}
-              <strong>pinyin</strong>, el <strong>vocabulario</strong> y las{" "}
-              <strong>estructuras básicas</strong> en <strong>8 semanas</strong>
-              , desarrollando la capacidad de mantener{" "}
-              <strong>conversaciones sencillas</strong> y de expresar{" "}
-              <strong>necesidades básicas</strong> en situaciones cotidianas.
-            </div>
-          </div>
-        }
-      />
+      <div className="flex flex-col items-center gap-y-[32px] px-[20%] py-[112px] w-full bg-[#ffffff] text-[var(--Text-primary,#000000)] font-['Roboto'] text-[16px] leading-[24px] tracking-[0px] font-[400] whitespace-normal">
+        <div className="self-stretch shrink-0 flex flex-col items-center gap-y-[24px] text-center">
+          <p className="text-[56px] leading-[67px] font-[700] self-stretch shrink-0">
+            Aprender idiomas no tiene por qué ser complicado.
+          </p>
+          <p className="text-[56px] leading-[67px] font-[700] self-stretch shrink-0">
+            学习语言无需复杂理论
+          </p>
 
-      <HomeCompose
-        place="left"
-        onClick={() => navigate("/course3")}
-        imgNode={
-          <HomeImg
-            src={caligra}
-            title={
-              <div className="text-[2rem] line-[2rem] text-right">
-                <span>
-                  La belleza de la cultura tradicional china -- Caligrafía
-                </span>
-                <br />
-                <span>中国传统文化之美——书法</span>
-                <br />
+          <p className="text-[18px] leading-[27px] self-stretch shrink-0">
+            En LEONASTUDYGRAM utilizamos el método de “entrada comprensible”
+            para enseñar chino y español, facilitando una adquisición rápida y
+            efectiva. Con nuestras clases en línea, puedes aprender desde
+            cualquier lugar y en cualquier momento. Nuestro servicio único
+            incluye un acompañamiento personalizado y supervisión diaria para
+            asegurar que cada estudiante complete con éxito su proceso de
+            aprendizaje.
+          </p>
+          <p className="text-[18px] leading-[27px] self-stretch shrink-0">
+            LEONASTUDYGRAM以“可理解输入法”教学中文与西班牙语，帮助学生高效入门。无论您身在何处，线上教学让学习随时随地变得轻松。我们的特色服务包括全程陪伴式支持，每日学业监督，确保每位学生都能稳步跟上学习节奏，真正实现语言学习目标。
+          </p>
+          <p className="text-[26px] leading-[30px] font-[700] self-stretch shrink-0">
+            ¡Empieza hoy tu camino hacia el aprendizaje de idiomas con facilidad
+            y éxito!
+          </p>
+          <p className="text-[26px] leading-[30px] font-[700] self-stretch shrink-0">
+            开始您的语言学习之旅，体验轻松高效的成长！
+          </p>
+        </div>
+
+        <div className="flex gap-[50px]">
+          <p className="text-left text-[15px] leading-[27px] self-stretch shrink-0 max-w-[400px] card shadow rounded-lg">
+            <strong>Resumen de características:</strong>
+            <br />
+            {[
+              "Aprendizaje efectivo: Método científico para dominar los fundamentos rápidamente.",
+              "Flexibilidad total: Clases en línea adaptadas a tu ritmo y horario.",
+              "Acompañamiento constante: Supervisión diaria que te ayuda a mantener el progreso.",
+            ].map((i) => (
+              <div className="flex text-left">{i}</div>
+            ))}
+          </p>
+          <p className="text-left flex-1 min-w-[400px] h-fit text-[15px] leading-[27px] self-stretch shrink-0  card shadow rounded-lg">
+            <strong>特色摘要：</strong>
+            <br />
+            {[
+              "高效入门：科学方法，让您快速掌握基础。",
+              "随时随地：线上教学灵活安排，无学习障碍。",
+              "全程陪伴：每日监督，助您坚持到底。",
+            ].map((i) => (
+              <div className="flex center text-left items-center">
+                <img src={strike} className="mr-2 w-[10px] h-[10px]" />
+                {i}
               </div>
+            ))}
+          </p>
+        </div>
+        <div className="shrink-0 inline-flex items-start gap-x-[16px] text-left">
+          <div
+            className="shrink-0 inline-flex justify-center items-center gap-x-[8px] px-[24px] py-[12px] bg-[var(--Color-Neutral-black,#000000)] outline-[1px] outline-[var(--Color-Neutral-black,#000000)] outline text-[var(--Text-alternate,#ffffff)]"
+            onClick={() =>
+              window.open(
+                "https://www.instagram.com/leonastudygram?igsh=YmUyN2RieHVrZWFj&utm_source=qr",
+                "_blank" // 在新标签页中打开
+              )
             }
-            caption={<></>}
-          />
-        }
-        contentNode={
-          <div className="max-w-[500px] flex-1 text-black pt-10">
-            <div className="text-lg font-bold">
-              Nombre del curso:{" "}
-              <strong>
-                Curso básico de caligrafia: Introducción alestilo
-                Kaishy(escritura regular)
-              </strong>{" "}
-              360€/人
-            </div>
-            <div className="text-lg mt-2">
-              <strong>Objetivo del curso:</strong> A través de un aprendizaje
-              sistemático, dominar los trazos yestructuras básicas del estilo
-              Kaishu desarrollando el interés porla caligrafia y mejorando las
-              habilidades de escritura. Este curso esideal para principiantes o
-              personas con conocimientos básicos encaligrafia.
-            </div>
+          >
+            <p className="shrink-0 min-w-[71px]">Inscríbete</p>
           </div>
-        }
-      />
+          <div
+            className="shrink-0 inline-flex justify-center items-center gap-x-[8px] px-[24px] py-[12px] outline-[1px] outline-[var(--Color-Neutral-black,#000000)] outline"
+            onClick={() => navigate("./course1")}
+          >
+            <p className="shrink-0 min-w-[63px]">Más Info</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-[80px]">
+        <HomeCompose
+          place="left"
+          onClick={() => navigate("/course1")}
+          imgNode={
+            <HomeImg
+              src={city}
+              title={
+                <>
+                  El curso de español
+                  <br />
+                  ideal para principiantes.
+                  <br />
+                  最适合初学者入门的西语课程
+                </>
+              }
+              caption={
+                <>
+                  ¿Por qué es importante aprender chino mandarín
+                  <br />
+                  para los niños...
+                </>
+              }
+            />
+          }
+          contentNode={
+            <div className="max-w-[550px] p-8 card shadow-lg text-black">
+              <h1 className="text-3xl font-bold mb-4">
+                Curso de escucha y conversación de español A1 de 4 Semanas
+              </h1>
+              <p className="text-xl text-brown-600 mb-4">€129</p>
+              <div className="flex items-center mb-4">
+                <span className="text-yellow-500">★★★★☆</span>
+                <span className="ml-2 text-gray-500">
+                  Ruta de aprendizaje única | Curso en línea
+                </span>
+              </div>
+              <p className="mb-4">
+                Comienza con los auténticos{" "}
+                <strong>hábitos lingüísticos</strong> de los nativos y practica
+                integralmente la <strong>comprensión auditiva</strong>, la{" "}
+                <strong>expresión oral</strong>, la <strong>lectura</strong> y
+                la <strong>escritura</strong>. Desde el principio, podrás
+                conversar.
+              </p>
+              <p className="mb-4">
+                <strong>Curso en línea</strong> en grupos pequeños de alta
+                calidad, con un máximo de <strong>10 personas</strong>, donde se
+                supervisan mutuamente en el aprendizaje.
+              </p>
+              <p>
+                Partiendo de <strong>textos de noticias</strong> o{" "}
+                <strong>historias de 10 minutos</strong>, completar en{" "}
+                <strong>cuatro semanas</strong> los temas básicos del nivel A1:
+                <strong> saludar</strong>,{" "}
+                <strong>preguntar sobre el clima</strong>,{" "}
+                <strong>preguntar sobre el estado de salud</strong> y expresar{" "}
+                <strong>actividades</strong> que uno mismo u otras personas
+                están realizando.
+              </p>
+            </div>
+          }
+        />
+        <HomeCompose
+          place="right"
+          onClick={() => navigate("/course2")}
+          imgNode={
+            <HomeImg
+              src={panda}
+              title={
+                <div className="text-[2rem] line-[2rem] text-right">
+                  <span>
+                    Aprender chino adaptadoal pensamiento de laslenguas
+                    occidentales.
+                  </span>
+                  <br />
+                  <span>适合西方语言思维的中文学习</span>
+                  <br />
+                </div>
+              }
+              caption={<></>}
+            />
+          }
+          contentNode={
+            <div className="max-w-[300px] flex-1 text-black pt-10 pl-10">
+              <div className="text-lg font-bold">
+                Nombre del curso:{" "}
+                <strong>
+                  Curso de Introducción al Chino para Principiantes
+                </strong>{" "}
+                520€/人
+              </div>
+
+              <div className="text-lg mt-2">
+                <strong>Objetivo del curso:</strong> Ayudar a los estudiantes
+                sin <strong>conocimientos previos</strong> de chino a dominar el{" "}
+                <strong>pinyin</strong>, el <strong>vocabulario</strong> y las{" "}
+                <strong>estructuras básicas</strong> en{" "}
+                <strong>8 semanas</strong>, desarrollando la capacidad de
+                mantener <strong>conversaciones sencillas</strong> y de expresar{" "}
+                <strong>necesidades básicas</strong> en situaciones cotidianas.
+              </div>
+            </div>
+          }
+        />
+        <HomeCompose
+          place="left"
+          onClick={() => navigate("/course3")}
+          imgNode={
+            <HomeImg
+              src={caligra}
+              title={
+                <div className="text-[2rem] line-[2rem] text-right">
+                  <span>
+                    La belleza de la cultura tradicional china -- Caligrafía
+                  </span>
+                  <br />
+                  <span>中国传统文化之美——书法</span>
+                  <br />
+                </div>
+              }
+              caption={<></>}
+            />
+          }
+          contentNode={
+            <div className="max-w-[500px] flex-1 text-black pt-10">
+              <div className="text-lg font-bold">
+                Nombre del curso:{" "}
+                <strong>
+                  Curso básico de caligrafia: Introducción alestilo
+                  Kaishy(escritura regular)
+                </strong>{" "}
+                360€/人
+              </div>
+              <div className="text-lg mt-2">
+                <strong>Objetivo del curso:</strong> A través de un aprendizaje
+                sistemático, dominar los trazos yestructuras básicas del estilo
+                Kaishu desarrollando el interés porla caligrafia y mejorando las
+                habilidades de escritura. Este curso esideal para principiantes
+                o personas con conocimientos básicos encaligrafia.
+              </div>
+            </div>
+          }
+        />
+      </div>
 
       {/* articles */}
       <div className="flex flex-col items-center gap-y-[80px] px-[64px] py-[112px] w-full font-['Roboto'] font-[700] text-left whitespace-normal tracking-[0px]">
